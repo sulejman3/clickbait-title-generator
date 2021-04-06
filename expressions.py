7a65d9b5ac411b4abe1bfcc8a4819726f6a9e805
vasi1 <vasi1@koumakan>
2021-04-06 21:13:41 +0200

    better organization *	all functions are defined in expressions.py *	app.py does basic stuff

--- expressions.py
+++ expressions.py
@@ -1,13 +1,40 @@
+#!/usr/bin/python3
+
 import random
+import expressions as e
+import json
 
+def dict_gen(database):
+    with open(database, "r") as file:
+        db = json.load(file)
+    
+    op_db = {}
+    for family_key, family_values in db.items():
+        op_wordtype = {}
+        for wordtype_key, wordtype_values in family_values.items():
+            i = random.randint(0, len(wordtype_values) -1 )
+            rolled_word = wordtype_values[i]
+            op_wordtype[wordtype_key] = rolled_word
+        op_db[family_key] = op_wordtype
+    return op_db
+
+
+# example output:
 # My brain hurts! I ate too much shit!
-def butthurt(human_part, verb, content):
-    print(f"My {human_part} hurts! I {verb} too much {content}.")
+def butthurt(db):
+    words = db["words"]
+    print(f"My {words['human part']} hurts! I {words['verb']} too much {words['content']}.")
 
+
+# example output:
 # A teacher ate 5 blocks of shit for 5 days. See what happened to their brain!
-def shitpost(person, verb, obj, content, time_unit, human_part):
-    print(f"A {person} {verb} {str(random.randint(1,10))} {obj} of {content} for {str(random.randint(1,10))} {time_unit}. See what happened to their {human_part}!")
+def shitpost(db):
+    words = db['words']
+    units = db['units']
+    num1 = str(random.randint(1,10))
+    num2 = str(random.randint(1,10))
+    print(f"A {words['person']} {words['verb']} {num1} {words['object']} of {words['content']} for {num2} {units['time']}. See what happened to their {words['human part']}!")
 
-expressions = [
-    butthurt, shitpost
-]
+
+if __name__ == "__main__":
+    print(dict_gen("cb_library.json"))
